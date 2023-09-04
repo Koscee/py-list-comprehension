8c1cc037a5615d877ad171965d520e8eafcb036a
Koscee <52253099+Koscee@users.noreply.github.com>
2023-09-05 01:20:31 +0800

    fix(nato_phonetic): handle exception when user input numbers

--- dictionary_comprehension/nato_alphabet.py
+++ dictionary_comprehension/nato_alphabet.py
@@ -6,7 +6,17 @@ nato_alphabet_df = pandas.read_csv('nato_phonetic_alphabet.csv')
 nato_phonetic_dict = {row.letter: row.code for (_index, row) in nato_alphabet_df.iterrows()}
 print(nato_phonetic_dict)
 
+
 # Create a list of the phonetic code words from a word that user inputs
-word = input("Enter a word: ").upper()
-output_list = [nato_phonetic_dict[letter] for letter in word]
-print(output_list)
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [nato_phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+
+generate_phonetic()
